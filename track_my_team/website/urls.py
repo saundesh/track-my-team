994f5bb4a12438a7d307d19b337c78cfa354e5a6
Shirley Huang <shuan102@illinois.edu>
2017-11-12 22:48:47 -0600

    create team form complete

--- track_my_team/website/urls.py
+++ track_my_team/website/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^captain/$', views.captain, name='captain'),
     url(r'^player/$', views.player, name='player'),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^login/$', views.login, name='login')
+    url(r'^login/$', views.login, name='login'),
+    url(r'^captain/create-team/$', views.create_team, name='create-team')
 ]
--- track_my_team/website/views.py
+++ track_my_team/website/views.py
@@ -21,3 +21,6 @@ def signup(request):
 
 def login(request):
     return render(request, 'website/login.html')
+
+def create_team(request):
+    return render(request, 'website/create-team.html')
