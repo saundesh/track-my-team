1f3dc245bcd192046336416df22321b90d261fba
Shirley Huang <shuan102@illinois.edu>
2017-11-13 09:08:57 -0600

    add models for database objects

--- track_my_team/website/admin.py
+++ track_my_team/website/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import User, Team, Player
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Team)
+admin.site.register(Player)
--- track_my_team/website/models.py
+++ track_my_team/website/models.py
@@ -4,3 +4,31 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+# User model will store account information
+class User(models.Model):
+    email = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.email
+
+# Team model will store team profile information
+class Team(models.Model):
+    team_name = models.CharField(max_length=64)
+    sport = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
+    zip_code = models.IntegerField()
+
+    def __str__(self):
+        return self.team_name
+
+# Player model will store player profile information
+class Player(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    phone_number = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
