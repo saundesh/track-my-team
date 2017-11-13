d1044c8e570d014ada36d7eff7d1260c9263300f
Shirley Huang <shuan102@illinois.edu>
2017-11-13 01:20:53 -0600

    added roster form and updated player page

--- track_my_team/website/urls.py
+++ track_my_team/website/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^captain/$', views.captain, name='captain'),
-    url(r'^player/$', views.player, name='player'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', views.login, name='login'),
-    url(r'^captain/create-team/$', views.create_team, name='create-team')
+    url(r'^captain/$', views.captain, name='captain'),
+    url(r'^captain/create-team/$', views.create_team, name='create-team'),
+    url(r'^captain/create-roster/$', views.create_roster, name='create-roster'),
+    url(r'^player/$', views.player, name='player'),
+    url(r'^player/team/$', views.team_profile, name='team-profile'),
+    url(r'^player/team/roster/$', views.team_roster, name='team-roster')
 ]
--- track_my_team/website/views.py
+++ track_my_team/website/views.py
@@ -10,17 +10,26 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'website/home.html')
 
-def captain(request):
-    return render(request, 'website/captain.html')
-
-def player(request):
-    return render(request, 'website/player.html')
-
 def signup(request):
     return render(request, 'website/signup.html')
 
 def login(request):
     return render(request, 'website/login.html')
 
+def captain(request):
+    return render(request, 'website/captain.html')
+
 def create_team(request):
     return render(request, 'website/create-team.html')
+
+def create_roster(request):
+    return render(request, 'website/create-roster.html')
+
+def player(request):
+    return render(request, 'website/player.html')
+
+def team_profile(request):
+    return render(request, 'website/team-profile.html')
+
+def team_roster(request):
+    return render(request, 'website/team-roster.html')
